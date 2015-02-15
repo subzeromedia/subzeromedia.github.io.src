edc3144758656bdf32f9db1823458623660a0227
Nik G <subzeromedia@gmail.com>
2015-02-15 19:35:16 +0000

    images added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,3 +6,31 @@ $('#previewModal').on('show.bs.modal', function (event) {
   modal.find('.modal-image').attr("src", imgUrl);
   modal.find('.modal-title').text(title);
 })
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel(
+    {
+      items : 2,
+    itemsCustom : false,
+    itemsDesktop : [1199,4],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [768,2],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+    singleItem : false,
+    itemsScaleUp : false,
+      navigation : false,
+      navigationText : ["prev","next"],
+      rewindNav : true,
+      scrollPerPage : false,
+      //Pagination
+    pagination : true,
+    paginationNumbers: false,
+ 
+    // Responsive 
+    responsive: true,
+    responsiveRefreshRate : 200,
+    responsiveBaseWidth: window,
+    }
+   );
+});
